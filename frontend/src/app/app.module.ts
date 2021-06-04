4c1a3b7965b5eca7713dee21f33af0748431c7d3
Ashutosh Raula <ashutosh7004@gmail.com>
2021-06-04 12:42:04 +0530

    New Feature

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -18,8 +18,9 @@ import { AlertifyService } from './services/alertify.service';
 import { AuthService } from './services/auth.service';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
-
-
+import { TabsModule } from 'ngx-bootstrap/tabs';
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 
 const appRoutes:Routes=[
   {path: '', component: PropertyListComponent},
@@ -43,6 +44,7 @@ const appRoutes:Routes=[
       UserLoginComponent,
       UserRegisterComponent,
       PropertyContactComponent
+
    ],
   imports: [
     BrowserModule,
@@ -51,7 +53,10 @@ const appRoutes:Routes=[
     ReactiveFormsModule,
     RouterModule.forRoot(appRoutes),
     BrowserAnimationsModule,
-    BsDropdownModule.forRoot()
+    BsDropdownModule.forRoot(),
+    TabsModule.forRoot(),
+    ButtonsModule.forRoot(),
+    BsDatepickerModule.forRoot()
 
   ],
   providers: [
--- frontend/src/app/property/add-property/add-property.component.ts
+++ frontend/src/app/property/add-property/add-property.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
+import { TabsetComponent } from 'ngx-bootstrap/tabs/public_api';
+import { IPropertyBase } from 'src/app/model/ipropertybase';
+
 
 
 @Component({
@@ -10,15 +13,36 @@ import { Router } from '@angular/router';
 })
 export class AddPropertyComponent implements OnInit {
 @ViewChild('Form') addPropertForm:NgForm;
+@ViewChild('formTabs') formTabs: TabsetComponent;
+
+propertyTypes:Array<string> =['House', 'Appartment', 'Duplex']
+furnishTypes:Array<string> =['Fully', 'Semi', 'Unfurnished']
+readytoMove:Array<string>=['Yes','No']
+gatedCommunity:Array<string>=['Yes','No']
+readyToMoveenws:Array<string>=['East','West','South','North']
+
+
+propertyView: IPropertyBase = {
+  Id: null,
+  Name: '',
+  Price: null,
+  SellRent: null,
+  PType:null,
+  FType:null,
+  BHK:null,
+  BuiltArea:null,
+  City:null,
+  RTM:null
+};
+
   constructor(private router: Router) { }
 
   ngOnInit() {
-    //this.addPropertForm.controls['Name'].setValue('Default Value');
-    setTimeout(()=>{
-      this.addPropertForm.controls['Name'].setValue('Default Value');
-    },1000)
-  }
 
+  }
+  selectTab(tabId: number) {
+    this.formTabs.tabs[tabId].active = true;
+  }
   onBack(){
     this.router.navigate(['/']);
   }
